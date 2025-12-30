c5e24eb1181faf8ec23f55a9b9519ef7817dec6d
AllanK24 <kazakovallan4@gmail.com>
2025-12-30 10:22:30 +0300

    PeftModel.from_pretrained else fallback for QuanTA fixed

--- quanta/quanta/peft_model.py
+++ quanta/quanta/peft_model.py
@@ -160,7 +160,11 @@ class PeftModel(PushToHubMixin, torch.nn.Module):
                 )
             model = dispatch_model(model, device_map=device_map)
             hook = AlignDevicesHook(io_same_device=True)
-            if model.peft_config.peft_type == PeftType.LORA or model.peft_config.peft_type == PeftType.BOTTLENECK:
+            if (
+                model.peft_config.peft_type == PeftType.LORA
+                or model.peft_config.peft_type == PeftType.BOTTLENECK
+                or model.peft_config.peft_type == PeftType.QUANTA
+            ):
                 add_hook_to_module(model.base_model.model, hook)
             else:
                 remove_hook_from_submodules(model.prompt_encoder)
